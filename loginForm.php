653a67518d43898d7653ea5a26ba16bae053d4e7
Ionlylike1kindofcheese <99066333@mydavinci.nl>
2023-03-27 09:23:43 +0200

    Lab 3 - Formulier (V1.1 done)

---
+++ loginForm.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Formulier V1.1</title>
+  <link rel="stylesheet" href="loginForm.css">
+</head>
+<body>
+  <div id="container">
+    <h1>De ingevulde gegevens zijn:</h1>
+    E-mail: <?php $email = '';
+    if (strpos($_POST["email"], '@') && strpos($_POST["email"], '.')) {
+      if (strpos($_POST["email"], '<') || strpos($_POST["email"], '>')) {
+        $email = '';
+      } else {
+        $email = $_POST["email"];
+      }
+    }
+    echo $email; ?><br>
+    Wachtwoord: <?php echo '*******'; ?>
+  </div>
+</body>
+</html>
