744dba514d26ada53d6e73ead26ce6864adf0fc9
Ionlylike1kindofcheese <99066333@mydavinci.nl>
2023-03-27 12:47:24 +0200

    Lab 3 - Formulier (V2.0 Done)

--- loginForm.php
+++ loginForm.php
@@ -4,22 +4,39 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Formulier V1.2</title>
-  <link rel="stylesheet" href="loginForm.css">
+  <title>Formulier V2.0</title>
 </head>
 <body>
-  <div id="container">
-    <h1>De ingevulde gegevens zijn:</h1>
-    E-mail: <?php $email = '';
-    if (strpos($_POST["email"], '@') && strpos($_POST["email"], '.')) {
-      if (strpos($_POST["email"], '<') || strpos($_POST["email"], '>')) {
-        $email = '';
-      } else {
-        $email = $_POST["email"];
+  <form method="post">
+    <div class="container">
+      <label for="email"><b>E-mail: </b></label>
+      <input type="text" placeholder="E-mail *" name="email" required>
+  
+      <label for="password"><b>Wachtwoord: </b></label>
+      <input type="password" placeholder="Wachtwoord *" name="password" required>
+  
+      <button type="submit" name="submitButton">Login</button>
+    </div>
+  </form>
+  <div class="container">
+    <?php
+    if (isset($_POST['submitButton'])) {
+      $email = '';
+      if (strpos($_POST["email"], '@') && strpos($_POST["email"], '.')) {
+        if (strpos($_POST["email"], '<') || strpos($_POST["email"], '>')) {
+          $email = '';
+        } else {
+          $email = $_POST["email"];
+        }
       }
+      echo "<h1>De ingevulde gegevens zijn:</h1>";
+      echo "E-mail: " . $email . "<br>";
+      echo "Wachtwoord: *******";
     }
-    echo $email; ?><br>
-    Wachtwoord: <?php echo '*******'; ?>
+    ?>
   </div>
+  <style>
+  <?php include "loginForm.css"; ?>
+  </style>
 </body>
 </html>
